1b9fc2b506c29a80ac2a99c2d939e3c69b3c602d
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-10 19:28:25 -0500

    Api

--- internal/master/api.go
+++ internal/master/api.go
@@ -1,1 +1,37 @@
 package master
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type taskRequest struct {
+	Command string `json:"command"`
+}
+
+func (s *NodeServer) AddTaskHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req taskRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid json body", http.StatusBadRequest)
+		return
+	}
+
+	if req.Command == "" {
+		http.Error(w, "command is required", http.StatusBadRequest)
+		return
+	}
+
+	s.CmdChannel <- req.Command
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	_ = json.NewEncoder(w).Encode(map[string]string{
+		"status":  "queued",
+		"command": req.Command,
+	})
+}
